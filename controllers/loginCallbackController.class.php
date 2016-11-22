262a6539cdef418e0e2cf67744e951355b2e6164
Reconnois <GUI.QUIRIN@hotmail.fr>
2016-11-22 21:40:05 +0100

    Suppression de la page login

--- controllers/loginCallbackController.class.php
+++ controllers/loginCallbackController.class.php
@@ -9,13 +9,15 @@ class loginCallbackController extends template{
 		
 		$helper = $this->fb->getRedirectLoginHelper();
 
-		try {
+		try{
 			$accessToken = $helper->getAccessToken();
-		} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		} 
+		catch(Facebook\Exceptions\FacebookResponseException $e) {
 		  // When Graph returns an error
 			echo 'Graph returned an error: ' . $e->getMessage();
 			exit;
-		} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		}
+		catch(Facebook\Exceptions\FacebookSDKException $e) {
 		  // When validation fails or other local issues
 			echo 'Facebook SDK returned an error: ' . $e->getMessage();
 			exit;
