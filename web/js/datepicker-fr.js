183d67ddb23d7ad682a0f0ad316aa5fac3286602
DESKTOP-L4UH6IU\lguib <l.guibert@hotmail.fr>
2016-12-06 12:55:59 +0100

    Ajout CSS/JS DatePicker

---
+++ web/js/datepicker-fr.js
@@ -0,0 +1,12 @@
+;(function($){
+	$.fn.datepicker.dates['fr'] = {
+		days: ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"],
+		daysShort: ["dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam.", "dim."],
+		daysMin: ["dim", "lun", "mar", "mer", "jeu", "ven", "sam", "dim"],
+		months: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+		monthsShort: ["Janv.", "Févr.", "Mars", "Avril", "Mai", "Juin", "Juil.", "Août", "Sept.", "Oct.", "Nov.", "Déc."],
+		today: "Aujourd'hui",
+		clear: "Effacer",
+		weekStart: 1
+	};
+}(jQuery));
