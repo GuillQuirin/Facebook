0336b214c075a5e513b6e53a201d78751ad0f1d1
DESKTOP-L4UH6IU\lguib <l.guibert@hotmail.fr>
2016-12-06 12:58:20 +0100

    Mise à jour page noCompetition et CSS

--- views/noCompetition.php
+++ views/noCompetition.php
@@ -1,8 +1,14 @@
 <header>
 </header>
-
-<div>Pas de compétition ouverte actuellement !</div>
-
-<footer>
-	<?php echo $setting->getAdress(); ?>
+<body>
+	<div class="padding-left-30">
+		<h2 class="text-uppercase">Pas de concours actuellement !</h2>
+		<h2>Revenez plus tard ...</h2>
+	</div>
+</body>
+<footer class="footer">
+	<div class="col-md-6">
+		<img src="https://scontent-fra3-1.xx.fbcdn.net/v/t1.0-9/552345_420640654657180_1666928990_n.jpg?oh=7e0262fb4fa4671e45c13bfefcbfc4ef&oe=58C27523">
+		<label>Pardon Maman Tatoo & Piercing <?php echo $setting->getAdress(); ?></label>
+	</div>
 </footer>
