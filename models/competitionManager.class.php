d840c38aa6d7ba0ded842ac1409569b3bb5205e0
Reconnois <GUI.QUIRIN@hotmail.fr>
2016-12-07 14:06:20 +0100

    Correction de competition

--- models/competitionManager.class.php
+++ models/competitionManager.class.php
@@ -18,7 +18,7 @@ class competitionManager extends basesql{
 
 	public function getAllCompetitions(){
     	$sql = "SELECT c.id_competition, c.name, c.description, c.start_date,
-    					c.end_date, c.prize, c.id_winner, c.active,
+    					c.end_date, c.prize, c.url_prize, c.id_winner, c.active,
     					COUNT(p.id_competition) as totalParticipants 
     			FROM ".$this->table." c 
     			LEFT OUTER JOIN participate p ON c.id_competition=p.id_competition
