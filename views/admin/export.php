c62e64505b389bfc9bcac56ee35a9aeb96ceddf7
DESKTOP-L4UH6IU\lguib <l.guibert@hotmail.fr>
2016-12-28 14:09:32 +0100

    Création de la vue export.php accessible depuis la partie admin

---
+++ views/admin/export.php
@@ -0,0 +1,20 @@
+ <div class="container">
+  <div class="row">
+    <div class="col-md-3 bord">
+      <h2>PARTICIPANTS</h2>
+      <div class="col-md-6"><a class="btn btn-warning" href="<?= WEBPATH ?>/export/exportUser" role="button">CSV <span class="glyphicon glyphicon-export" aria-hidden="true"></span></a></div>
+      <div class="col-md-6"><a class="btn btn-warning" href="<?= WEBPATH ?>/export/exportUserXML" role="button">XML <span class="glyphicon glyphicon-export" aria-hidden="true"></span></a></div>
+      <p></p>
+    </div>
+    <div class="col-md-3 col-md-offset-1 bord">
+      <h2>CONCOURS</h2>
+      <div class="col-md-6"><a class="btn btn-warning" href="<?= WEBPATH ?>/export/exportCompetition" role="button">CSV <span class="glyphicon glyphicon-export" aria-hidden="true"></span></a></div>
+      <div class="col-md-6"><a class="btn btn-warning" href="<?= WEBPATH ?>/export/exportCompetitionXML" role="button">XML <span class="glyphicon glyphicon-export" aria-hidden="true"></span></a></div>
+    </div>
+    <div class="col-md-3 col-md-offset-1 bord">
+      <h2>TOUTE LA BASE</h2>
+       <div class="col-md-6"><a class="btn btn-warning" href="#" role="button">CSV <span class="glyphicon glyphicon-export" aria-hidden="true"></span></a></div>
+      <div class="col-md-6"><a class="btn btn-warning" href="#" role="button">XML <span class="glyphicon glyphicon-export" aria-hidden="true"></span></a></div>
+    </div>
+  </div>
+</div>
