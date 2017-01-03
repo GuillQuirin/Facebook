12b1908634c00529ee3205e915df0973e7b32dc6
Elise <elisepoirier@outlook.com>
2017-01-03 12:40:56 +0100

    jai oublié un truc

--- views/noCompetition.php
+++ views/noCompetition.php
@@ -1,17 +1,15 @@
-<header>
-</header>
-<body>
-	<div class="padding-left-30">
-		<h2 class="text-uppercase">Pas de concours actuellement !</h2>
-		<h2>Revenez plus tard ...</h2>
-	</div>
-</body>
-<footer class="footer">
-	<div class="col-md-6">
+
+<div class="text-right">
+	<h2 class="text-uppercase">Pas de concours actuellement !</h2>
+	<h2>Revenez plus tard ...</h2>
+</div>
+<div class="footer">
+	<div class="col-xs-6">
 		<img src="https://scontent-fra3-1.xx.fbcdn.net/v/t1.0-9/552345_420640654657180_1666928990_n.jpg?oh=7e0262fb4fa4671e45c13bfefcbfc4ef&oe=58C27523">
-		<label>Pardon Maman Tatoo & Piercing <?php echo $setting->getAdress(); ?></label>
+		<label>Pardon Maman Tatoo <br> & Piercing <?php echo $setting->getAdress(); ?></label>
 	</div>
-	<div class="hidden-sm col-md-offset-4">
+
+	<div class="hidden-sm col-sx-offset-4">
 		<?php 
 		if(isset($user)){ //Utilisateur connecté
 			if(in_array($user->getId(),$listAdmins))//Administrateur
@@ -23,4 +21,4 @@
 			echo '<a href="'.$urlLoginLogout.'">Se connecter</a>';
 		?>
 	</div>
-</footer>
+</div>
