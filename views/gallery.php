fae70650fe8e4d16bba38f81b2f6808520d84204
Reconnois <GUI.QUIRIN@hotmail.fr>
2017-01-04 00:16:05 +0100

    Insertion auto des données dans la modal

--- views/gallery.php
+++ views/gallery.php
@@ -1,12 +1,15 @@
 <?php 
 	if(!empty($listParticipation)): 
 		?>
+		<!-- Nombre d'élèments à afficher --> 
 		<select name="quantity">
 			<option value="2">2</option>
 			<option value="5">5</option>
 			<option value="10" selected>10</option>
 			<option value="20">20</option>
 		</select>
+
+		<!-- Tri des élèments -->
 		<select name="sort">
 			<option value="0" selected>Aléatoire</option>
 			<option value="1">Le plus ancien</option>
@@ -14,10 +17,36 @@
 			<option value="3">Tri par like</option>
 		</select>
 		<input type="hidden" id="url" value="<?php echo __URI__; ?>">
+		
+		<!-- Mosaique -->
 		<div class="row">
 			<div class="col-md-12" id="gallery">
 			</div>
 		</div>
+
+		<!-- Modal -->
+		<div class="modal fade" id='popUpGallery' tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		  <div class="modal-dialog" role="document">
+		    <form action="<?php echo WEBPATH.'/index/submit'; ?>" method="post">
+		    <div class="modal-content">
+		      <div class="modal-header">
+			    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		        <h4>Photo proposée par <span class="modal-name"></span>.</h4> 
+		      </div>
+		      <div class="modal-body">
+		      </div>
+		      <div class="modal-footer">
+		      	<div class="col-xs-6 col-sm-6 col-md-6 modal-report report text-left">
+		      		<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/VisualEditor_-_Icon_-_Alert.svg/2000px-VisualEditor_-_Icon_-_Alert.svg.png" alt="Signaler"> Signaler la photographie
+		        </div>
+		        <div class="col-md-6 modal-like text-right">
+				</div>
+		      </div>
+		    </div>
+		    </form>
+		  </div>
+		</div>
+
 		<!-- Pagination -->
 		<div class="row">
 			<nav class="col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2" aria-label="Page navigation">
--- web/js/gallery.js
+++ web/js/gallery.js
@@ -29,8 +29,11 @@ function getContent(page){
 		},
 		url: "gallery/getGallery", 
 		success: function(result){
-			var listParticipation = JSON.parse(result);
+
+			$("#loading").remove();
+
 			//Tri par like décroissant
+			var listParticipation = JSON.parse(result);
 			if($('select[name="sort"]').val()=="3"){
 				listParticipation.sort(function(a, b) {
 				    return parseInt(b.nb_likes) - parseInt(a.nb_likes);
@@ -39,27 +42,38 @@ function getContent(page){
 
 			var nbPages = 1;
 			var code = "";
-			$("#loading").hide();
 			var i = 0;
+			
 			$.each(listParticipation,function(){
 				if(i>=depart && i<arrivee){
 					code += "<div id='"+this.id+"' class='col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-0 col-md-4'>";
-						code += "<figure style='background-image:url("+this.url_photo+")'>";
-							//code += "<img src='"+this.url_photo+"'>";
-							code += "<figcaption>";
-								code += '<p class="report col-xs-2 col-sm-2 col-md-2"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/VisualEditor_-_Icon_-_Alert.svg/2000px-VisualEditor_-_Icon_-_Alert.svg.png" alt="Signaler"></p>';
-								code += '<p class="user text-center col-xs-6 col-sm-6 col-md-6">'+this.first_name+' '+this.last_name+' '+this.nb_likes+'</p>';
-								code += '<div class="fb-like col-xs-4 col-sm-4 col-md-4" data-href="'+this.url_photo_cleaned+'" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="false"></div>';				
-							code += '</figcaption>';
+
+						code += "<figure ";
+								code += "data-toggle='modal'";
+								code += "data-target='#popUpGallery'";
+								code += "data-url='"+this.url_photo+"'";
+								code += "data-name='"+this.first_name+" "+this.last_name+"'";
+								code += "data-like='"+this.url_photo_cleaned+"'";
+								code += "data-report='"+this.id+"'";
+							code += "style='background-image:url("+this.url_photo+")'>";
+
+								code += "<figcaption>";
+									code += '<p class="report col-xs-2 col-sm-2 col-md-2"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/VisualEditor_-_Icon_-_Alert.svg/2000px-VisualEditor_-_Icon_-_Alert.svg.png" alt="Signaler"></p>';
+									code += '<p class="user text-center col-xs-6 col-sm-6 col-md-6">'+this.first_name+' '+this.last_name+' '+this.nb_likes+'</p>';
+									code += '<div class="fb-like col-xs-4 col-sm-4 col-md-4" data-href="'+this.url_photo_cleaned+'" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="false"></div>';				
+								code += '</figcaption>';
+
 						code += "</figure>";
 					code += "</div>";
 				}
 				i++;
 			});
 			
+			//Mise à jour de la mosaïque
 			$('#gallery').html(code);
 			FB.XFBML.parse();
 
+			//Pagination
 			code = "";
 			for(i=1; i<=Math.ceil(listParticipation.length/qtty); i++){
 				code += "<li";
@@ -69,6 +83,7 @@ function getContent(page){
 			$('ul.pagination').html(code);
 			
 			//Appel du code pour signaler APRES le load du contenu ajax
+			openModal();
 			report();
 			like();
 		},
@@ -84,11 +99,24 @@ function like(){
 	});*/
 }
 
+function openModal(){
+	$('#popUpGallery').not('figcaption').on('show.bs.modal', function (event) {
+		var button = $(event.relatedTarget); // Button that triggered the modal
+
+		var modal = $(this);
+		modal.find('.modal-name').text(button.data('name'));
+		modal.find('.modal-body').css('background-image','url('+button.data("url")+')');
+		modal.find('.modal-report img').attr('idImage',button.data('report'));
+		modal.find('.modal-like').html('<div class="fb-like" data-href="'+button.data('like')+'" data-layout="box_count" data-action="like" data-size="large" data-show-faces="true" data-share="false"></div>');
+		FB.XFBML.parse();
+	});
+}
+
 function report(){
 	//Signalement
 	$('.report').on("click",function(){
 		if(confirm("Souhaitez-vous vraiment signaler cette image ?")){
-			id = $(this).parent().parent().parent().prop('id'); 
+			id = ($(this).attr('idImage')==undefined) ? $(this).parent().parent().parent().prop('id') : $(this).attr("idImage"); 
 			$.ajax({method: "POST",
 				data:{
 					id : id
@@ -99,6 +127,7 @@ function report(){
 					$("#"+id+" .report").addClass("reportSent").html("Signalement envoyé.");
 				}
 			});
+			console.log(id);
 		}
 	});
 }
