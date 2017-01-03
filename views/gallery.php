c7632f98610415e46cc671c1c45876f3440101ea
Reconnois <GUI.QUIRIN@hotmail.fr>
2017-01-03 22:54:30 +0100

    Pagination + Responsive de la Galerie

--- views/gallery.php
+++ views/gallery.php
@@ -1,6 +1,12 @@
 <?php 
 	if(!empty($listParticipation)): 
 		?>
+		<select name="quantity">
+			<option value="2">2</option>
+			<option value="5">5</option>
+			<option value="10" selected>10</option>
+			<option value="20">20</option>
+		</select>
 		<select name="sort">
 			<option value="0" selected>Aléatoire</option>
 			<option value="1">Le plus ancien</option>
@@ -8,28 +14,17 @@
 			<option value="3">Tri par like</option>
 		</select>
 		<input type="hidden" id="url" value="<?php echo __URI__; ?>">
-		<div class="col-md-12" id="gallery">
+		<div class="row">
+			<div class="col-md-12" id="gallery">
+			</div>
 		</div>
 		<!-- Pagination -->
-		<nav aria-label="Page navigation">
-		  <ul class="pagination">
-		    <li>
-		      <a href="#" aria-label="Previous">
-		        <span aria-hidden="true">&laquo;</span>
-		      </a>
-		    </li>
-		    <li><a href="#">1</a></li>
-		    <li><a href="#">2</a></li>
-		    <li><a href="#">3</a></li>
-		    <li><a href="#">4</a></li>
-		    <li><a href="#">5</a></li>
-		    <li>
-		      <a href="#" aria-label="Next">
-		        <span aria-hidden="true">&raquo;</span>
-		      </a>
-		    </li>
-		  </ul>
-		</nav>
+		<div class="row">
+			<nav class="col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2" aria-label="Page navigation">
+			  <ul class="pagination">
+			  </ul>
+			</nav>
+		</div>
 <?php else: ?>
 	<h3>Soyez le premier participant au concours !</h3>
 	<h5>Pour vous inscrire, accèder <a href="<?php echo WEBPATH.'/index'; ?>">ici</a></h5>
--- web/js/gallery.js
+++ web/js/gallery.js
@@ -1,72 +1,88 @@
 $(document).ready(function(){
-	getContent();
+	getContent(1);
 
 	//Tri
 	$('select[name="sort"]').change(function(){
-		getContent();
+		getContent(1);
 	});
 
-	//Pagination
-	/*$('select[name="sort"]').change(function(){
+	//Quantité
+	$('select[name="quantity"]').change(function(){
 		getContent(1);
-	});*/
+	});
 });
 
-function getContent(){
+function getContent(page){
 	//Méthode de tri
 	var tri = $('select[name="sort"]').val();
+
+	//Méthode de pagination
+	var qtty = $('select[name="quantity"]').val();
+	var depart = (page-1) * qtty;
+	var arrivee = parseInt(depart)+parseInt(qtty);
+
 	$('#gallery').html('<img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif" alt="Chargement" id="loading">');
 
-	setTimeout(function(){
-			$.ajax({method: "POST",
-				data:{
-					tri : tri
-				},
-				url: "gallery/getGallery", 
-				success: function(result){
-					var listParticipation = JSON.parse(result);
-					
-					//Tri par like décroissant
-					if($('select[name="sort"]').val()=="3"){
-						listParticipation.sort(function(a, b) {
-						    return parseInt(b.nb_likes) - parseInt(a.nb_likes);
-						});
-					}
+	$.ajax({method: "POST",
+		data:{
+			tri : tri
+		},
+		url: "gallery/getGallery", 
+		success: function(result){
+			var listParticipation = JSON.parse(result);
+			//Tri par like décroissant
+			if($('select[name="sort"]').val()=="3"){
+				listParticipation.sort(function(a, b) {
+				    return parseInt(b.nb_likes) - parseInt(a.nb_likes);
+				});
+			}
 
-					var code = "";
-					$("#loading").hide();
-					$.each(listParticipation,function(){
-						code += "<div id='"+this.id+"' class='col-xs-12 col-sm-6 col-md-4'>";
-							code += "<figure>";
-								code += "<img class='img-thumbnail' src='"+this.url_photo+"'>";
-								code += "<figcaption>";
-									code += '<p class="report col-xs-2 col-sm-2 col-md-2"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/VisualEditor_-_Icon_-_Alert.svg/2000px-VisualEditor_-_Icon_-_Alert.svg.png" alt="Signaler"></p>';
-									code += '<p class="user text-center col-xs-6 col-sm-6 col-md-6">'+this.first_name+' '+this.last_name+' '+this.nb_likes+'</p>';
-									code += '<div class="fb-like col-xs-4 col-sm-4 col-md-4" data-href="'+this.url_photo_cleaned+'" data-layout="button_count" data-action="like" data-size="large" data-show-faces="false" data-share="false"></div>';				
-								code += '</figcaption>';
-							code += "</figure>";
-						code += "</div>";
-					});
-					$('#gallery').html(code);
-					FB.XFBML.parse();
-					//dispPagination();
-					//Appel du code pour signaler APRES le load du contenu ajax
-					report();
-					like();
-				},
-				fail: function(){
-					console.log('Pas OK');
+			var nbPages = 1;
+			var code = "";
+			$("#loading").hide();
+			var i = 0;
+			$.each(listParticipation,function(){
+				if(i>=depart && i<arrivee){
+					code += "<div id='"+this.id+"' class='col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-0 col-md-4'>";
+						code += "<figure style='background-image:url("+this.url_photo+")'>";
+							//code += "<img src='"+this.url_photo+"'>";
+							code += "<figcaption>";
+								code += '<p class="report col-xs-2 col-sm-2 col-md-2"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/VisualEditor_-_Icon_-_Alert.svg/2000px-VisualEditor_-_Icon_-_Alert.svg.png" alt="Signaler"></p>';
+								code += '<p class="user text-center col-xs-6 col-sm-6 col-md-6">'+this.first_name+' '+this.last_name+' '+this.nb_likes+'</p>';
+								code += '<div class="fb-like col-xs-4 col-sm-4 col-md-4" data-href="'+this.url_photo_cleaned+'" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="false"></div>';				
+							code += '</figcaption>';
+						code += "</figure>";
+					code += "</div>";
 				}
+				i++;
 			});
-		}, 1000);
+			
+			$('#gallery').html(code);
+			FB.XFBML.parse();
+
+			code = "";
+			for(i=1; i<=Math.ceil(listParticipation.length/qtty); i++){
+				code += "<li";
+					if(i==page) code += " class='active' ";
+				code += "><a href='#' onclick='getContent("+i+")'>"+i+"</a></li>";
+			}
+			$('ul.pagination').html(code);
+			
+			//Appel du code pour signaler APRES le load du contenu ajax
+			report();
+			like();
+		},
+		fail: function(){
+			console.log('Pas OK');
+		}
+	});
 }
 
 function like(){
-	$('.fb-like').on('click',function(){
-		getContent();
-	});
+	/*$('.fb-like').on('click',function(){
+		getContent($(".pagination li.active a").text());
+	});*/
 }
-
 
 function report(){
 	//Signalement
@@ -86,16 +102,3 @@ function report(){
 		}
 	});
 }
-
-	/*
-		foreach($listParticipation as $key => $participation){
-		$result .= '<div class="col-md-3">';
-			$result .= '<div class="thumbnail">';
-				$result .= '<img src="'.$participation->getUrl_photo().'" alt="Photo de concours">';
-				$result .= '<div class="caption">';
-					$result .= '<p>123 likes</p>';
-				$result .= '</div>';
-			$result .= '</div>';
-		$result .= '</div>';
-		}
-	*/
