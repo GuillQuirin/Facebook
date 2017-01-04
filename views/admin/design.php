f89337e9f1d1a45f1d07e4d8c019e2b8c324a952
DESKTOP-L4UH6IU\lguib <l.guibert@hotmail.fr>
2017-01-04 09:02:17 +0100

    Mise à jour formulaire création de design et aperçu

--- views/admin/design.php
+++ views/admin/design.php
@@ -2,14 +2,21 @@
 	<div class="col-md-5">
 		<h2 class="text-uppercase"> Sélection d'un design existant </h2>
 		<div class="input-group">
-			<select class="form-control" name="designExist" id="designExist">                        
+			<select class="form-control" name="designExist" id="designExist">
+				<?php foreach ($listDesign as $key => $design) { ?>
+				<option value="<?php echo $design->getId_design();?>"><?php echo $design->getName(); ?></option>
+				<?php } ?>                            
 			</select>
 			<span class="input-group-btn">
 				<button type="submit" class="btn btn-success">Appliquer le design</button>
 			</span>
 		</div><br>
-
+		<hr>
 		<h2 class="text-uppercase"> Création d'un design </h2>
+		<div class="form-group">
+				<label for="name">Nom du design :</label>
+				<input type="text" class="form-control" name="name" id="name" placeholder="" required>
+		</div>
 		<div class="form-group">
 			<label for="url">URL de l'image de fond :</label>
 			<input type="text" class="form-control" name="url" id="url" placeholder="">
@@ -21,14 +28,13 @@
 			<label for="color">Couleur du fond :</label>
 			<input type="text" class="form-control" name="color" id="color">
 		</div>
-		<hr>
 		<div class="form-group">
 			<label for="fontColor">Couleur de la police :</label>
 			<input type="text" class="form-control" name="fontColor" id="fontColor" required>
 		</div>
 		<div class="form-group">
-			<label for="fontColor">Couleur des champs du formulaire :</label>
-			<input type="text" class="form-control" name="formColor" id="formColor" required>
+			<label for="fontColorForm">Couleur des champs du formulaire :</label>
+			<input type="text" class="form-control" name="fontColorForm" id="fontColorForm" required>
 		</div>
 		<div class="form-group">
 			<label for="fontSize">Taille de la police :</label>
@@ -48,23 +54,24 @@
 			</select>
 		</div>
 		<button type="submit" class="btn btn-success">Appliquer le design</button>
-		<button type="submit" class="btn btn-success">Enregistrer sous un nouveau template</button>
+		<button type="button" data-toggle="modal" data-target="#newTemplate"  class="btn btn-success">Enregistrer sous un nouveau template</button>
 	</div>
 	<div class="col-md-6 col-md-offset-1">
 		<h2 class="text-uppercase">Aperçu</h2>
-		<div class="col-md-12 apercu">
+		<div class="col-md-12 apercu" id="apercu">
 			<h2 class="text-uppercase"> Sélection d'un design existant </h2>
 			<div class="input-group">
-				<select class="form-control" name="designExist" id="designExist"> 
-					<?php foreach ($listDesign as $key => $design) : ?>
-                        <option value="<?= $design->getName();?>"><?php echo $design->getName(); ?></option>
-                    <?php endforeach; ?>                        
+				<select class="form-control" name="designExistExemple" id="designExistExemple">                       
 				</select>
 				<span class="input-group-btn">
 					<button type="submit" class="btn btn-success">Appliquer le design</button>
 				</span>
 			</div><br>
 			<h2 class="text-uppercase"> Création d'un design </h2>
+			<div class="form-group">
+				<label for="name">Nom du design :</label>
+				<input type="text" class="form-control" name="url" id="url" placeholder="" required>
+			</div>
 			<div class="form-group">
 				<label for="url">URL de l'image de fond :</label>
 				<input type="text" class="form-control" name="url" id="url" placeholder="" required>
@@ -75,7 +82,7 @@
 			</div> 
 			<div class="form-group">
 				<label for="fontStyle">Police du texte :</label>
-				<select class="form-control" name="designExist" id="designExist" required>
+				<select class="form-control" name="fontStyle" id="fontStyle" required>
 					<option value="">Arial</option>                               
 				</select>
 			</div>
@@ -84,3 +91,30 @@
 		</div>
 	</div>
 </div>
+<script>
+	$(document).ready(function () {
+		$('select[name="designExist"]').change(function () {
+			$.ajax({
+				url: 'design/getDesign',
+				method: 'POST',
+				data: {id_design: $(this).val()},
+				success: function (data) {
+					console.log(data);
+					var listDesign = JSON.parse(data);
+					document.getElementById("name").value = listDesign[0].name;
+					document.getElementById("url").value = listDesign[0].background_url;
+					document.getElementById("color").value = listDesign[0].background_color;
+					document.getElementById("fontColor").value = listDesign[0].text_color;
+					document.getElementById("fontColorForm").value = listDesign[0].form_color;
+					document.getElementById("fontSize").value = listDesign[0].font_size;
+					document.getElementById("fontStyle").value = listDesign[0].font_name;
+					document.getElementById("apercu").style.backgroundColor = listDesign[0].background_color;
+					document.getElementById("apercu").style.color = listDesign[0].text_color;
+					document.getElementById("apercu").style.fontSize = listDesign[0].font_size;
+					document.getElementById("apercu").style.fontFamily = listDesign[0].font_name;
+					document.getElementById("apercu").style.backgroundImage = "url('" + listDesign[0].background_url + "')";
+				}
+			});
+		});
+	});
+</script>
