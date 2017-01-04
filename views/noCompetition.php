f1d02ddfbc7be4915b022fccfc5cf1c55e615e22
Elise <elisepoirier@outlook.com>
2017-01-04 14:27:55 +0100

    integration noCompetition

--- views/noCompetition.php
+++ views/noCompetition.php
@@ -1,15 +1,15 @@
 
-<div class="text-right">
+<div>
 	<h2 class="text-uppercase">Pas de concours actuellement !</h2>
 	<h2>Revenez plus tard ...</h2>
 </div>
 <div class="footer">
 	<div class="col-xs-6">
 		<img src="https://scontent-fra3-1.xx.fbcdn.net/v/t1.0-9/552345_420640654657180_1666928990_n.jpg?oh=7e0262fb4fa4671e45c13bfefcbfc4ef&oe=58C27523">
-		<label>Pardon Maman Tatoo <br> & Piercing <?php echo $setting->getAdress(); ?></label>
+		<p>Pardon Maman <br> Tatoo & Piercing <?php echo $setting->getAdress(); ?></p>
 	</div>
 
-	<div class="hidden-sm col-sx-offset-4">
+	<div class="col-xs-12 text-right admin-connexion">
 		<?php 
 		if(isset($user)){ //Utilisateur connecté
 			if(in_array($user->getId(),$listAdmins))//Administrateur
@@ -18,7 +18,7 @@
 			echo '<a href="'.WEBPATH.'/logout">Se déconnecter</a>';
 		}
 		else
-			echo '<a href="'.$urlLoginLogout.'">Se connecter</a>';
+			echo '<a href="'.$urlLoginLogout.'">Connection administrateur</a>';
 		?>
 	</div>
 </div>
