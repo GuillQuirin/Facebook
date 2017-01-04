38615d5ec9b231c18374e3265489d32de1fc1c04
DESKTOP-L4UH6IU\lguib <l.guibert@hotmail.fr>
2017-01-04 09:01:41 +0100

    Requête selection design selon son Id

--- models/designManager.class.php
+++ models/designManager.class.php
@@ -17,4 +17,23 @@ class designManager extends basesql{
 		}
 		return $list;
 	}
+
+	public function insertDesign(design $data){
+		$this->save($data);
+	} 
+
+	public function getDesignById($id){
+		$sql = "SELECT * FROM design WHERE id_design = :id_design";
+		$table = [
+    		'id_design' => $id
+    	];	
+		$sth = $this->pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+		$sth->execute($table);
+		$r = $sth->fetchAll(PDO::FETCH_ASSOC);
+		$list = [];;
+		foreach ($r as $key => $value) {
+			$list[] = $value;
+		}
+		return $list;
+	}
 }
