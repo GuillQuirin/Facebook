3109407aaff6245c513c2d3febfdd5b874f79f4f
DESKTOP-L4UH6IU\lguib <l.guibert@hotmail.fr>
2017-01-07 12:21:54 +0100

    Ajout des requêtes permettant de récupérer et de mettre à jour le design actif

--- models/designManager.class.php
+++ models/designManager.class.php
@@ -36,4 +36,34 @@ class designManager extends basesql{
 		}
 		return $list;
 	}
+
+	public function getActiveDesign(){
+		$sql = "SELECT * FROM design WHERE state = 1";
+		$sth = $this->pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+		$sth->execute();
+		$r = $sth->fetchAll(PDO::FETCH_ASSOC);
+		$list = [];
+		foreach ($r as $key => $value) {
+			$list[] = new design($value);
+		}
+		return $list;
+	}
+
+	public function activeDesign($id){
+		$sql = "UPDATE design SET state = 1 WHERE id_design = :id_design";
+		$table = [
+    		'id_design' => $id
+    	];	
+		$sth = $this->pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+		$sth->execute($table);
+	}
+
+	public function inactiveDesign($id){
+		$sql = "UPDATE design SET state = 0 WHERE id_design <> :id_design";
+		$table = [
+    		'id_design' => $id
+    	];	
+		$sth = $this->pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+		$sth->execute($table);
+	}	
 }
