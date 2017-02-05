7bff06b1e6bbb7a5bc264fb2fb40e7c171d87a00
Reconnois <GUI.QUIRIN@hotmail.fr>
2017-02-05 20:18:16 +0100

    CGU

---
+++ controllers/CGUController.php
@@ -0,0 +1,14 @@
+<?php
+class CGUController extends template{
+	public function CGUAction(){
+		$v = new view();
+		$this->assignConnectedProperties($v);
+
+		$v->assign("css", "noCompetition");
+		$v->assign("js", "noCompetition");
+		$v->assign("title", "Conditions Générales");
+		$v->assign("setting", $setting);
+
+        $v->setView("noCompetition", "templateempty");
+    }
+}
