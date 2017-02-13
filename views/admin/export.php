00278e651c6239c7075789e6d1146534659800b8
DESKTOP-L4UH6IU\lguib <l.guibert@hotmail.fr>
2017-02-13 21:03:48 +0100

    Mise à jour du design page "export"

--- views/admin/export.php
+++ views/admin/export.php
@@ -1,16 +1,14 @@
  <div class="container">
-  <div class="row">
-    <div class="col-md-4 col-md-offset-4 bord">
-      <h2>Utilisateurs</h2>
-      <div class="col-md-6"><a class="btn btn-warning" href="<?php echo WEBPATH; ?>/export/exportUser" role="button">CSV <span class="glyphicon glyphicon-export" aria-hidden="true"></span></a></div>
-      <div class="col-md-6"><a class="btn btn-warning" href="<?php echo WEBPATH; ?>/export/exportUserXML" role="button">XML <span class="glyphicon glyphicon-export" aria-hidden="true"></span></a></div>
+  <div class="row" id="export">
+    <div class="col-sm-4 col-sm-offset-1 bord">
+      <h2>PARTICIPANTS</h2>
+      <div class="col-sm-6"><a class="btn btn-warning" href="<?php echo WEBPATH; ?>/export/exportUser" role="button">CSV <span class="glyphicon glyphicon-export" aria-hidden="true"></span></a></div>
+      <div class="col-sm-6"><a class="btn btn-warning" href="<?php echo WEBPATH; ?>/export/exportUserXML" role="button">XML <span class="glyphicon glyphicon-export" aria-hidden="true"></span></a></div>
     </div>
-  </div><br>
-  <div class="row">
-    <div class="col-md-4 col-md-offset-4 bord">
+    <div class="col-sm-4 col-sm-offset-2 bord">
       <h2>CONCOURS</h2>
-      <div class="col-md-6"><a class="btn btn-warning" href="<?php echo WEBPATH; ?>/export/exportCompetition" role="button">CSV <span class="glyphicon glyphicon-export" aria-hidden="true"></span></a></div>
-      <div class="col-md-6"><a class="btn btn-warning" href="<?php echo WEBPATH; ?>/export/exportCompetitionXML" role="button">XML <span class="glyphicon glyphicon-export" aria-hidden="true"></span></a></div>
+      <div class="col-sm-6"><a class="btn btn-warning" href="<?php echo WEBPATH; ?>/export/exportCompetition" role="button">CSV <span class="glyphicon glyphicon-export" aria-hidden="true"></span></a></div>
+      <div class="col-sm-6"><a class="btn btn-warning" href="<?php echo WEBPATH; ?>/export/exportCompetitionXML" role="button">XML <span class="glyphicon glyphicon-export" aria-hidden="true"></span></a></div>
     </div>
   </div><br>
   <!-- <div class="row">
