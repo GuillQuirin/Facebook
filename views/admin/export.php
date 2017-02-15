54f05a0b58cb2d3c6e42d45a76bafa90d778243c
Reconnois <GUI.QUIRIN@hotmail.fr>
2017-02-15 10:55:27 +0100

    export

--- views/admin/export.php
+++ views/admin/export.php
@@ -20,7 +20,7 @@
     </a>
   </div>
 </div>
-<h2 class="text-center">TOUTE LA BASE</h2>
+<!-- <h2 class="text-center">TOUTE LA BASE</h2>
 <div class="row bord">
   <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
     <a class="btn btn-warning" href="<?php echo WEBPATH; ?>/export/exportParticipant" role="button">CSV <span class="glyphicon glyphicon-export" aria-hidden="true"></span>
@@ -30,4 +30,4 @@
     <a class="btn btn-warning" href="<?php echo WEBPATH; ?>/export/exportParticipantXML" role="button">XML <span class="glyphicon glyphicon-export" aria-hidden="true"></span>
     </a>
   </div>
-</div>
+</div> -->
