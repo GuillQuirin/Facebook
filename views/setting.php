7e3a1e990749fa5490016fe7bd2746e54f64cda6
Reconnois <GUI.QUIRIN@hotmail.fr>
2017-02-15 10:14:21 +0100

    coucou

--- views/setting.php
+++ views/setting.php
@@ -1,11 +1,11 @@
 <div class="form-group">
   	<h1 class="text-uppercase">Règlement du concours </h1>
-  	<textarea class="summernote" id="regulation"><?php echo $listSetting[1]->getValue();?></textarea> 
+  	<textarea class="summernote" id="regulation"><?php (isset($listSetting)) ? echo $listSetting[1]->getValue() : "";?></textarea> 
   	<button type="submit" id="saveRegulation" class="btn btn-success">Sauvegarder le règlement</button>
 </div><br>
 <div class="form-group">
   	<h1 class="text-uppercase">Conditions Générales d'Utilisation </h1>
-  	<div class="summernote" id="cgu"><?php echo $listSetting[0]->getValue();?></div>
+  	<div class="summernote" id="cgu"><?php echo (isset($listSetting)) ? $listSetting[0]->getValue() : "";?></div>
   	<button type="submit" id="saveCGU" class="btn btn-success">Sauvegarder les CGU</button>
 </div>
 <script>
