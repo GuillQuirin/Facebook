8f2687afbc9d4d3a0ab552068e9ffcc81ee0fa08
DESKTOP-L4UH6IU\lguib <l.guibert@hotmail.fr>
2017-02-15 12:33:22 +0100

    Classe bootstrap sur gallery

--- views/gallery.php
+++ views/gallery.php
@@ -1,27 +1,30 @@
 <?php if(isset($competition) && $competition!==NULL) : ?>
 	<?php if(isset($listParticipation) && !empty($listParticipation)): ?>
 		<div class="col-sm-12 selectitem">
+			<div class="col-sm-2">
 			<!-- Nombre d'élèments à afficher --> 
-			<select name="quantity">
+			<select class="form-control" name="quantity">
 				<option value="2">2</option>
 				<option value="5">5</option>
 				<option value="10" selected>10</option>
 				<option value="20">20</option>
 			</select>
-
+			</div>
+			<div class="col-sm-3">
 			<!-- Tri des élèments -->
-			<select name="sort">
+			<select class="form-control" name="sort">
 				<option value="0" selected>Aléatoire</option>
 				<option value="1">Le plus ancien</option>
 				<option value="2">Le plus récent</option>
 				<option value="3">Tri par like</option>
 			</select>
+			</div>
 		</div>
 		<input type="hidden" id="url" value="<?php echo __URI__; ?>">
 
 		<!-- Mosaique -->
 		<div class="row">
-			<div class="col-md-12" id="gallery">
+			<div class="col-sm-12" id="gallery">
 			</div>
 		</div>
 
@@ -46,7 +49,7 @@
 
 		<!-- Pagination -->
 		<div class="row">
-			<nav class="text-center col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2" aria-label="Page navigation">
+			<nav class="text-center col-sm-12" aria-label="Page navigation">
 				<ul class="pagination">
 				</ul>
 			</nav>
